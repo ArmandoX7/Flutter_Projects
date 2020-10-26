fb1bff71472987c29cc55952efd557a69a2876af
ArmandoX7 <jesus.aguirre@tectijuana.edu.mx>
2020-10-26 09:15:47 -0700

    se subio proyecto

---
+++ talla/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:talla/src/pages/calculaTalla_page.dart';
+
+void main() {
+  runApp(CalculaTallaPage());
+}
---
+++ talla/lib/src/pages/calculaTalla_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:talla/src/pages/jeans_page.dart';
+import 'package:talla/src/pages/cinturilla_page.dart';
+import 'package:talla/src/pages/short_page.dart';
+import 'package:talla/src/pages/body_page.dart';
+
+class CalculaTallaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: 'Jeans',
+                ),
+                Tab(
+                  text: 'Cinturilla',
+                ),
+                Tab(
+                  text: 'Short',
+                ),
+                Tab(
+                  text: 'Body',
+                ),
+              ],
+            ),
+            title: Text('Calcula tu tulla'),
+            centerTitle: true,
+          ),
+          body: TabBarView(
+            children: [
+              ListView(
+                children: <Widget>[JeansPage()],
+              ),
+              ListView(
+                children: <Widget>[CinturillaPage()],
+              ),
+              ListView(
+                children: <Widget>[ShortPage()],
+              ),
+              ListView(
+                children: <Widget>[BodyPage()],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
