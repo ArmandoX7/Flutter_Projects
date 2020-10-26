edb194f4c15d40acda9ef5ea9b77dfeaaacf67ec
ArmandoX7 <jesus.aguirre@tectijuana.edu.mx>
2020-10-26 10:59:39 -0700

    update home_page.dart

---
+++ peliculas/lib/src/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[_swiperTarjetas()],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
